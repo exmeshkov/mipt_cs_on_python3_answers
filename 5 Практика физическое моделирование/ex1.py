a5b2090c6f99f9b557fadd4f0b85862b67413904
exmeshkov <exmeshkov@gmail.com>
2021-03-01 20:42:06 +0300

    added sun and changed the calculation of coordinates

--- 5 Практика физическое моделирование/ex1.py
+++ 5 Практика физическое моделирование/ex1.py
@@ -1,16 +1,21 @@
 import graphics as gr
 
-SIZE_X = 400
-SIZE_Y = 400
+SIZE_X = 800
+SIZE_Y = 800
 
 window = gr.GraphWin("Model", SIZE_X, SIZE_Y)
 
-coords = gr.Point(200, 200)
-velocity = gr.Point(1, -2)
-acceleration = gr.Point(0, 0.1)
+coords = gr.Point(400, 700)
+velocity = gr.Point(2, 0)
+acceleration = gr.Point(0, 0)
+
 ball = gr.Circle(coords, 10)
 ball.setFill('red')
 ball.draw(window)
+
+sun = gr.Circle(gr.Point(400, 400), 50)
+sun.setFill('yellow')
+sun.draw(window)
 
 
 def add(point_1, point_2):
@@ -21,12 +26,6 @@ def add(point_1, point_2):
 def sub(point_1, point_2):
     new_point = gr.Point(point_1.x - point_2.x, point_1.y - point_2.y)
     return new_point
-
-
-def clear_window():
-    rectangle = gr.Rectangle(gr.Point(0, 0), gr.Point(SIZE_X, SIZE_Y))
-    rectangle.setFill('green')
-    rectangle.draw(window)
 
 
 def check_coords(coords, velocity):
@@ -41,16 +40,18 @@ def update_velocity(velocity, acceleration):
     return add(velocity, acceleration)
 
 
-def update_acceleration(ball_coords, center_cords):
-    diff = sub(ball_coords, center_cords)
+def update_acceleration(ball_coords, center_coords):
+    diff = sub(ball_coords, center_coords)
     distance_2 = (diff.x ** 2 + diff.y ** 2) ** (3 / 2)
+
     G = 2000
+
     return gr.Point(-diff.x * G / distance_2, -diff.y * G / distance_2)
 
 
 while True:
-    acceleration = update_acceleration(coords, gr.Point(400, 400))
+    acceleration = update_acceleration(ball.getCenter(), gr.Point(400, 400))
     velocity = update_velocity(velocity, acceleration)
-    check_coords(coords, velocity)
+    check_coords(ball.getCenter(), velocity)
     ball.move(velocity.x, velocity.y)
     gr.time.sleep(0.02)
